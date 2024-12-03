a7fd598e256d418b44f4f47c07b89659323b6b36
mharboziuk <nharboziuk@gmail.com>
2024-12-03 22:24:47 +0200

    third day and eslint/prettier :)

--- 3/1.js
+++ 3/1.js
@@ -1,19 +1,19 @@
-const path = require('path');
-const input = require('fs')
+const path = require("path");
+const input = require("fs");
 
-const file = input.readFileSync(path.join(__dirname, 'input1.txt'), 'utf8').trim();
+const file = input
+  .readFileSync(path.join(__dirname, "input1.txt"), "utf8")
+  .trim();
 
-
-const string = file.matchAll(/mul\([0-9]+,[0-9]+\)/ig);
+const string = file.matchAll(/mul\([0-9]+,[0-9]+\)/gi);
 
 let sum = 0;
 [...string].forEach(([mulString]) => {
-  const numbersString = mulString.matchAll(/[0-9]+/ig);
-  const array =  [...numbersString]
-  const mul = array[0][0] * array[1][0]
+  const numbersString = mulString.matchAll(/[0-9]+/gi);
+  const array = [...numbersString];
+  const mul = array[0][0] * array[1][0];
 
-  sum += mul
-
-})
+  sum += mul;
+});
 
 console.log(sum);
--- 3/2.js
+++ 3/2.js
@@ -1,50 +1,52 @@
-const path = require('path');
-const input = require('fs')
+const path = require("path");
+const input = require("fs");
 
-const file = input.readFileSync(path.join(__dirname, 'input2.txt'), 'utf8').trim();
+const file = input
+  .readFileSync(path.join(__dirname, "input2.txt"), "utf8")
+  .trim();
 
-const stringOfDo = file.matchAll(/do[^n\'t]/g);
-const stringOfDont = file.matchAll(/don\'t/g);
+const stringOfDo = file.matchAll(/do[^n't]/g);
+const stringOfDont = file.matchAll(/don't/g);
 
-const doIndexes = []
+const doIndexes = [];
 for (const match of stringOfDo) {
   doIndexes.push(match.index);
 }
 
-const dontIndexes = []
+const dontIndexes = [];
 for (const match of stringOfDont) {
   dontIndexes.push(match.index);
 }
 
-const stringOfMuls = file.matchAll(/mul\([0-9]+,[0-9]+\)/ig);
+const stringOfMuls = file.matchAll(/mul\([0-9]+,[0-9]+\)/gi);
 
 let sum = 0;
 let shouldMul = true;
 for (const match of stringOfMuls) {
-  const indexOfNumber = match.index
+  const indexOfNumber = match.index;
 
-  const currentDontIndex = dontIndexes.findIndex((a) => a < indexOfNumber)
+  const currentDontIndex = dontIndexes.findIndex((a) => a < indexOfNumber);
   if (currentDontIndex !== -1) {
-    shouldMul = false
-    dontIndexes.shift()
+    shouldMul = false;
+    dontIndexes.shift();
   }
 
-  const currentDoIndex = doIndexes.findIndex((a) => a < indexOfNumber)
+  const currentDoIndex = doIndexes.findIndex((a) => a < indexOfNumber);
 
   if (currentDoIndex !== -1) {
-    shouldMul = true
-    doIndexes.shift()
+    shouldMul = true;
+    doIndexes.shift();
   }
 
   if (!shouldMul) {
-    continue
+    continue;
   }
 
-  const numbersString = match[0].matchAll(/[0-9]+/ig);
-  const array =  [...numbersString]
-  const mul = array[0][0] * array[1][0]
+  const numbersString = match[0].matchAll(/[0-9]+/gi);
+  const array = [...numbersString];
+  const mul = array[0][0] * array[1][0];
 
-  sum += mul
+  sum += mul;
 }
 
 console.log(sum);
